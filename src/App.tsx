86bf7264dc34ce1fffd1b71a09e5391b8d2be79f
Ingrid Alkimim <ingridalkm@outlook.com>
2024-06-26 17:18:51 -0300

    Promps

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import React from 'react'
 import Home from './pages/Home'
 import './App.css';
 
+
 function App() {
-  return(
-    <>
-      <Home></Home>
-    </>
-  )
-  
+  return (
+   <Home  title="Componente Home"
+          description="Este é um componente Home que recebe props."/>
+  );
 }
 
 export default App
