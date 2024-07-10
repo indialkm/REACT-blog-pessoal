7f346d2bc8011c396ad5b6f566bc990be2ede6f5
Ingrid Alkimim <ingridalkm@outlook.com>
2024-07-10 11:31:09 -0300

    terminou enfim

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import homeLogo from '../../assets/home.png'
 import './Home.css';
+import ListaPostagens from '../../components/postagem/listaPostagem/ListaPostagem';
+import ModalPostagem from '../../components/postagem/modalPostagem/ModalPostagem';
+
+
 
 function Home() {
     return (
@@ -8,21 +13,21 @@ function Home() {
           <div className='container grid grid-cols-2 text-white'>
             <div className="flex flex-col gap-4 items-center justify-center py-4">
               <h2 className='text-5xl font-bold'>Seja bem vinde!</h2>
-              <p className='text-xl'>Expresse aqui seus pensamentos e opiniões</p>
+              <p className='text-xl'>Expresse aqui seus pensamentos e opniões</p>
   
               <div className="flex justify-around gap-4">
-              
-                <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
-              </div>
+              <ModalPostagem />
+              <button className='rounded bg-white text-blue-800 py-2 px-4'>Ver postagens</button>
+            </div>
             </div>
   
             <div className="flex justify-center ">
-             
+              <img src={homeLogo} alt="" className='w-2/3' />
       
             </div>
           </div>
         </div>
-        
+        <ListaPostagens />
       </>
     );
 }
